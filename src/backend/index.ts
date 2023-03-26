ef185c5ad692a4960450c3fa38d2a5969104e870
huydt7702 <developer.vanhuy@gmail.com>
2023-03-26 15:29:30 +0700

    config to run backend

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,1 +1,22 @@
-console.log("Backend");
+import express, { Request, Response } from "express";
+import path from "path";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "templates");
+
+app.use("/assets", express.static(path.join("dist/frontend")));
+app.use("/", express.static(path.join("public")));
+
+app.get("/board", (req: Request, res: Response) => {
+    res.render("index", {});
+});
+
+app.get("/login", (req: Request, res: Response) => {
+    res.render("login", {});
+});
+
+app.listen(3333, () => {
+    console.log(`Sever is running`);
+});
